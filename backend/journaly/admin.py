ff11ecc4ba21974895a95e6d340b5060cd9ed77c
JWAbayan <johnwpabayan@gmail.com>
2021-10-31 20:59:30 +0800

    Added User Model

--- backend/journaly/admin.py
+++ backend/journaly/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import User
+
+
+class JournalyAdmin(admin.ModelAdmin):
+    users_display = ('username', 'email', 'password')
+
 
 # Register your models here.
+admin.site.register(User, JournalyAdmin)
--- backend/journaly/models.py
+++ backend/journaly/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import EmailField
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True, null=False)
+    email = models.EmailField(null=False)
+    password = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.username
