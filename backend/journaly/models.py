3c1373ce69671592db0f7b0f26c7994ffdc02cba
Ivan Salvame <idcsalvame@gmail.com>
2021-11-01 14:41:32 +0800

    created model for tasks/events/notes

--- backend/journaly/models.py
+++ backend/journaly/models.py
@@ -11,3 +11,29 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class Item(models.Model):
+    type_option = (
+        ("T", "Task"),
+        ("E", "Event"),
+        ("N", "Note"),
+    )
+
+    status_option = (
+        ("C", "Completed"),
+        ("P", "Pending"),
+    )
+
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=1, choices=type_option)
+    title = models.CharField(max_length=100)
+    body = models.TextField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    journal = models.CharField(max_length=50, null=True, blank=True)
+    list = models.CharField(max_length=50, null=True, blank=True)
+    status = models.CharField(max_length=1, choices=status_option)
+
+    def __str__(self):
+        return self.id, self.title
